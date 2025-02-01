a34400e45beb04b5943801a6e5484b5356e8a4a4
NIDHIPATEL2807 <nidhipatelspit@gmail.com>
2025-02-01 10:36:21 +0530

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import MainLayout from './Layouts/MainLayout'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Correct import
+import './App.css';
+import MainLayout from './Layouts/MainLayout';
+import HomePage from './Pages/HomePage';
 
 function App() {
- 
-
   return (
     <>
-      
-      <MainLayout/>
+      <Router>
+        <Routes>
+          {/* MainLayout is the wrapper that contains other routes */}
+          <Route path="/" element={<MainLayout />}>
+            {/* HomePage will be rendered by default when accessing "/" */}
+            <Route index element={<HomePage />} /> 
+          </Route>
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
